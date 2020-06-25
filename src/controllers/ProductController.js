953092c9061e0234bc7fc5fe0a1dec8e7447cb60
Lucasedusi <lucasmto.16@hotmail.com>
2020-06-25 14:30:16 -0300

    New routes, and index, show, store, update, delete

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,32 @@ const Product = mongoose.model('Product');
 
 module.exports = {
   async index(req, res) {
-    const products = await Product.find();
+    const product = await Product.find();
+
+    return res.json(product);
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
+  async store(req, res) {
+    const products = await Product.create(req.body);
 
     return res.json(products);
+  },
+
+  async update(req, res) {
+    const products = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    return res.json(products);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
   },
 };
